a3017185417364af81c0a44c2b7d935cac61aeee
Igor Solkin <inbox@tomclaw.com>
2016-06-26 17:55:25 +0300

    Исправления lint-warnings.

--- app/src/main/java/com/tomclaw/appsend/SystemBarTintManager.java
+++ app/src/main/java/com/tomclaw/appsend/SystemBarTintManager.java
@@ -62,6 +62,7 @@ public class SystemBarTintManager {
      * @param activity The host activity.
      */
     @TargetApi(19)
+    @SuppressWarnings("ResourceType")
     public SystemBarTintManager(Activity activity) {
 
         Window win = activity.getWindow();
