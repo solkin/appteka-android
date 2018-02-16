ea51021a80ba3531c4b27d34c751b026d137e6e3
Igor Solkin <inbox@tomclaw.com>
2018-02-16 22:29:24 +0300

    Add new home activity

---
+++ app/src/main/java/com/tomclaw/appsend/main/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.tomclaw.appsend.main.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tomclaw.appsend.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
