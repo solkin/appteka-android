a419587f29919a1a800e08915420c72f12d4e9cb
Igor Solkin <inbox@tomclaw.com>
2018-06-29 22:21:06 +0300

    Add base local apps activity

---
+++ app/src/main/java/com/tomclaw/appsend/main/local/LocalAppsActivity.java
@@ -0,0 +1,11 @@
+package com.tomclaw.appsend.main.local;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.tomclaw.appsend.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.local_apps)
+public class LocalAppsActivity extends AppCompatActivity {
+}
