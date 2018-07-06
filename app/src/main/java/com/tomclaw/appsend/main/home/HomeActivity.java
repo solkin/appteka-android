53fcb255653e605f2c3197d38e56a26e6ed4c071
Igor Solkin <inbox@tomclaw.com>
2018-07-06 23:31:25 +0300

    Add new home screen logic

--- app/src/main/java/com/tomclaw/appsend/main/home/HomeActivity.java
+++ app/src/main/java/com/tomclaw/appsend/main/home/HomeActivity.java
@@ -1,15 +1,310 @@
 package com.tomclaw.appsend.main.home;
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tomclaw.appsend.R;
+import com.tomclaw.appsend.main.view.MemberImageView;
+import com.tomclaw.appsend.util.MemberImageHelper;
+
+import java.util.Random;
+
+import static com.tomclaw.appsend.util.MemberImageHelper.memberImageHelper;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private ImageView imgNavHeaderBg;
+    private MemberImageView imgProfile;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+
+    public static int navItemIndex = 0;
+
+    private static final String TAG_STORE = "store";
+    private static final String TAG_UPLOADS = "uploads";
+    private static final String TAG_DISCUSS = "discuss";
+    private static final String TAG_INSTALLED = "installed";
+    private static final String TAG_DISTRO = "distro";
+    public static String CURRENT_TAG = TAG_STORE;
+
+    private String[] activityTitles;
+
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHandler = new Handler();
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        fab = findViewById(R.id.fab);
+
+        navHeader = navigationView.getHeaderView(0);
+        txtName = navHeader.findViewById(R.id.name);
+        txtWebsite = navHeader.findViewById(R.id.website);
+        imgNavHeaderBg = navHeader.findViewById(R.id.img_header_bg);
+        imgProfile = navHeader.findViewById(R.id.img_profile);
+
+        activityTitles = new String[]{
+                getString(R.string.nav_store),
+                getString(R.string.nav_uploads),
+                getString(R.string.nav_discuss),
+                getString(R.string.nav_installed),
+                getString(R.string.nav_distro)
+        };
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        loadNavHeader();
+
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_STORE;
+            loadHomeFragment();
+        }
+    }
+
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        long userId = new Random().nextInt();
+        boolean isThreadOwner = userId == 1;
+
+        txtName.setText(memberImageHelper().getName(userId, isThreadOwner));
+        txtWebsite.setText(String.valueOf(userId));
+
+        imgProfile.setMemberId(userId);
+
+        // showing dot next to notifications label
+        navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
+    }
+
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        selectNavMenu();
+
+        setToolbarTitle();
+
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            toggleFab();
+            return;
+        }
+
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        mHandler.post(mPendingRunnable);
+
+        toggleFab();
+
+        drawer.closeDrawers();
+
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // Home Fragment
+                return new Fragment();
+            case 1:
+                // Uploads Fragment
+                return new Fragment();
+            case 2:
+                // Discuss Fragment
+                return new Fragment();
+            case 3:
+                // Installed Fragment
+                return new Fragment();
+            case 4:
+                // Distro Fragment
+                return new Fragment();
+            default:
+                // Home Fragment
+                return new Fragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_store:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_STORE;
+                        break;
+                    case R.id.nav_uploads:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_UPLOADS;
+                        break;
+                    case R.id.nav_discuss:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_DISCUSS;
+                        break;
+                    case R.id.nav_installed:
+                        navItemIndex = 3;
+                        CURRENT_TAG = TAG_INSTALLED;
+                        break;
+                    case R.id.nav_distro:
+                        navItemIndex = 4;
+                        CURRENT_TAG = TAG_DISTRO;
+                        break;
+                    case R.id.nav_settings:
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_info:
+                        drawer.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadHomeFragment();
+
+                return true;
+            }
+        });
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+            return;
+        }
+
+        if (shouldLoadHomeFragOnBackPress) {
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_STORE;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (navItemIndex == 0) {
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+        }
+
+        if (navItemIndex == 2) {
+            getMenuInflater().inflate(R.menu.main_discuss_menu, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.profile) {
+            Toast.makeText(getApplicationContext(), "User Profile", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleFab() {
+        if (navItemIndex == 0) {
+            fab.show();
+        } else {
+            fab.hide();
+        }
     }
 }
