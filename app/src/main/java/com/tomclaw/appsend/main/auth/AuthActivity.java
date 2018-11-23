cc08c26c6b9a54dc63a25a7969a2c79dc2aa6665
Igor Solkin <inbox@tomclaw.com>
2018-11-23 23:46:47 +0300

    Add base auth screen

---
+++ app/src/main/java/com/tomclaw/appsend/main/auth/AuthActivity.java
@@ -0,0 +1,18 @@
+package com.tomclaw.appsend.main.auth;
+
+import android.os.Bundle;
+
+import com.tomclaw.appsend.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth_activity);
+    }
+
+}
