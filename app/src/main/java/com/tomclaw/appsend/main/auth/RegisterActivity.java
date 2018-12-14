cccec4e0cb1ce55eaef27e0758a2580f8b153cea
Igor Solkin <inbox@tomclaw.com>
2018-12-14 22:54:33 +0300

    Add register activity dependencies

--- app/src/main/java/com/tomclaw/appsend/main/auth/RegisterActivity.java
+++ app/src/main/java/com/tomclaw/appsend/main/auth/RegisterActivity.java
@@ -2,11 +2,15 @@ package com.tomclaw.appsend.main.auth;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.tomclaw.appsend.R;
+import com.tomclaw.appsend.core.StoreServiceHolder;
+import com.tomclaw.appsend.net.Session;
 import com.tomclaw.appsend.util.ThemeHelper;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsItem;
@@ -20,6 +24,18 @@ import androidx.appcompat.widget.Toolbar;
 @SuppressLint("Registered")
 @EActivity(R.layout.register_activity)
 public class RegisterActivity extends AppCompatActivity {
+
+    @Bean
+    StoreServiceHolder serviceHolder;
+
+    @Bean
+    Session session;
+
+    @ViewById
+    EditText loginInput;
+
+    @ViewById
+    EditText passwordInput;
 
     @ViewById
     Toolbar toolbar;
