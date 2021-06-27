3c6a52174c04f73e348b3fcf46e66dff2d64d80d
Igor Solkin <inbox@tomclaw.com>
2021-06-27 23:11:08 +0300

    Refactor home distro fragment

--- app/src/main/java/com/tomclaw/appsend/main/local/HomeDistroFragment.java
+++ app/src/main/java/com/tomclaw/appsend/main/local/HomeDistroFragment.java
@@ -1,7 +1,11 @@
 package com.tomclaw.appsend.main.local;
 
+import static com.microsoft.appcenter.analytics.Analytics.trackEvent;
+import static com.tomclaw.appsend.util.IntentHelper.bluetoothApk;
+import static com.tomclaw.appsend.util.IntentHelper.openGooglePlay;
+import static com.tomclaw.appsend.util.IntentHelper.shareApk;
+
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.widget.ListAdapter;
@@ -25,11 +29,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static com.microsoft.appcenter.analytics.Analytics.trackEvent;
-import static com.tomclaw.appsend.util.IntentHelper.bluetoothApk;
-import static com.tomclaw.appsend.util.IntentHelper.openGooglePlay;
-import static com.tomclaw.appsend.util.IntentHelper.shareApk;
 
 @EFragment(R.layout.local_apps_fragment)
 public class HomeDistroFragment extends DistroFragment {
@@ -61,67 +60,64 @@ public class HomeDistroFragment extends DistroFragment {
     public void onClick(final ApkItem item) {
         ListAdapter menuAdapter = new MenuAdapter(getContext(), R.array.apk_actions_titles, R.array.apk_actions_icons);
         new AlertDialog.Builder(getContext())
-                .setAdapter(menuAdapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0: {
-                                setRefreshOnResume();
-                                installApp(item);
-                                trackEvent("click-install-apk");
-                                break;
+                .setAdapter(menuAdapter, (dialog, which) -> {
+                    switch (which) {
+                        case 0: {
+                            setRefreshOnResume();
+                            installApp(item);
+                            trackEvent("click-install-apk");
+                            break;
+                        }
+                        case 1: {
+                            shareApk(getContext(), new File(item.getPath()));
+                            trackEvent("click-share-apk");
+                            break;
+                        }
+                        case 2: {
+                            Intent intent = new Intent(getContext(), UploadActivity.class);
+                            intent.putExtra(UploadActivity.UPLOAD_ITEM, item);
+                            startActivity(intent);
+                            trackEvent("click-upload-apk");
+                            break;
+                        }
+                        case 3: {
+                            bluetoothApk(getContext(), new File(item.getPath()));
+                            trackEvent("click-bluetooth-share");
+                            break;
+                        }
+                        case 4: {
+                            final String packageName = item.getPackageName();
+                            openGooglePlay(getContext(), packageName);
+                            trackEvent("click-search-google-play");
+                            break;
+                        }
+                        case 5: {
+                            Intent intent = new Intent(getContext(), DownloadActivity.class);
+                            intent.putExtra(DownloadActivity.STORE_APP_PACKAGE, item.getPackageName());
+                            intent.putExtra(DownloadActivity.STORE_APP_LABEL, item.getLabel());
+                            startActivity(intent);
+                            trackEvent("click-search-appteka");
+                            break;
+                        }
+                        case 6: {
+                            try {
+                                PackageInfo packageInfo = item.getPackageInfo();
+                                List<String> permissions = Arrays.asList(packageInfo.requestedPermissions);
+                                PermissionsActivity_.intent(getContext())
+                                        .permissions(new PermissionsList(new ArrayList<>(permissions)))
+                                        .start();
+                            } catch (Throwable ex) {
+                                Snackbar.make(recycler, R.string.unable_to_get_permissions, Snackbar.LENGTH_LONG).show();
                             }
-                            case 1: {
-                                shareApk(getContext(), new File(item.getPath()));
-                                trackEvent("click-share-apk");
-                                break;
+                            break;
+                        }
+                        case 7: {
+                            File file = new File(item.getPath());
+                            if (file.delete()) {
+                                reloadFiles();
                             }
-                            case 2: {
-                                Intent intent = new Intent(getContext(), UploadActivity.class);
-                                intent.putExtra(UploadActivity.UPLOAD_ITEM, item);
-                                startActivity(intent);
-                                trackEvent("click-upload-apk");
-                                break;
-                            }
-                            case 3: {
-                                bluetoothApk(getContext(), new File(item.getPath()));
-                                trackEvent("click-bluetooth-share");
-                                break;
-                            }
-                            case 4: {
-                                final String packageName = item.getPackageName();
-                                openGooglePlay(getContext(), packageName);
-                                trackEvent("click-search-google-play");
-                                break;
-                            }
-                            case 5: {
-                                Intent intent = new Intent(getContext(), DownloadActivity.class);
-                                intent.putExtra(DownloadActivity.STORE_APP_PACKAGE, item.getPackageName());
-                                intent.putExtra(DownloadActivity.STORE_APP_LABEL, item.getLabel());
-                                startActivity(intent);
-                                trackEvent("click-search-appteka");
-                                break;
-                            }
-                            case 6: {
-                                try {
-                                    PackageInfo packageInfo = item.getPackageInfo();
-                                    List<String> permissions = Arrays.asList(packageInfo.requestedPermissions);
-                                    PermissionsActivity_.intent(getContext())
-                                            .permissions(new PermissionsList(new ArrayList<>(permissions)))
-                                            .start();
-                                } catch (Throwable ex) {
-                                    Snackbar.make(recycler, R.string.unable_to_get_permissions, Snackbar.LENGTH_LONG).show();
-                                }
-                                break;
-                            }
-                            case 7: {
-                                File file = new File(item.getPath());
-                                if (file.delete()) {
-                                    reloadFiles();
-                                }
-                                trackEvent("click-delete-app");
-                                break;
-                            }
+                            trackEvent("click-delete-app");
+                            break;
                         }
                     }
                 }).show();
