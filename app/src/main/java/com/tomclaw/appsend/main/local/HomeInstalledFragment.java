24936a3707c11a09ff2b69354768390fcd91c490
Igor Solkin <inbox@tomclaw.com>
2021-06-27 23:11:22 +0300

    Refactor home installed fragment

--- app/src/main/java/com/tomclaw/appsend/main/local/HomeInstalledFragment.java
+++ app/src/main/java/com/tomclaw/appsend/main/local/HomeInstalledFragment.java
@@ -1,7 +1,9 @@
 package com.tomclaw.appsend.main.local;
 
+import static com.microsoft.appcenter.analytics.Analytics.trackEvent;
+import static com.tomclaw.appsend.util.IntentHelper.openGooglePlay;
+
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -28,9 +30,6 @@ import org.androidannotations.annotations.EFragment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static com.microsoft.appcenter.analytics.Analytics.trackEvent;
-import static com.tomclaw.appsend.util.IntentHelper.openGooglePlay;
 
 @EFragment(R.layout.local_apps_fragment)
 public class HomeInstalledFragment extends InstalledFragment {
@@ -63,88 +62,85 @@ public class HomeInstalledFragment extends InstalledFragment {
     private void showActionDialog(final AppItem item) {
         ListAdapter menuAdapter = new MenuAdapter(getContext(), R.array.app_actions_titles, R.array.app_actions_icons);
         new AlertDialog.Builder(getContext())
-                .setAdapter(menuAdapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0: {
-                                PackageManager packageManager = getContext().getPackageManager();
-                                Intent launchIntent = packageManager.getLaunchIntentForPackage(item.getPackageName());
-                                if (launchIntent == null) {
-                                    Snackbar.make(recycler, R.string.non_launchable_package, Snackbar.LENGTH_LONG).show();
-                                } else {
-                                    startActivity(launchIntent);
-                                }
-                                trackEvent("click-run-app");
-                                break;
+                .setAdapter(menuAdapter, (dialog, which) -> {
+                    switch (which) {
+                        case 0: {
+                            PackageManager packageManager = getContext().getPackageManager();
+                            Intent launchIntent = packageManager.getLaunchIntentForPackage(item.getPackageName());
+                            if (launchIntent == null) {
+                                Snackbar.make(recycler, R.string.non_launchable_package, Snackbar.LENGTH_LONG).show();
+                            } else {
+                                startActivity(launchIntent);
                             }
-                            case 1: {
-                                TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_SHARE));
-                                trackEvent("click-share-apk");
-                                break;
+                            trackEvent("click-run-app");
+                            break;
+                        }
+                        case 1: {
+                            TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_SHARE));
+                            trackEvent("click-share-apk");
+                            break;
+                        }
+                        case 2: {
+                            TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_EXTRACT));
+                            trackEvent("click-extract-apk");
+                            break;
+                        }
+                        case 3: {
+                            Intent intent = new Intent(getContext(), UploadActivity.class);
+                            intent.putExtra(UploadActivity.UPLOAD_ITEM, item);
+                            startActivity(intent);
+                            trackEvent("click-upload-apk");
+                            break;
+                        }
+                        case 4: {
+                            TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_BLUETOOTH));
+                            trackEvent("click-bluetooth-share");
+                            break;
+                        }
+                        case 5: {
+                            String packageName = item.getPackageName();
+                            openGooglePlay(getContext(), packageName);
+                            trackEvent("click-search-google-play");
+                            break;
+                        }
+                        case 6: {
+                            Intent intent = new Intent(getContext(), DownloadActivity.class);
+                            intent.putExtra(DownloadActivity.STORE_APP_PACKAGE, item.getPackageName());
+                            intent.putExtra(DownloadActivity.STORE_APP_LABEL, item.getLabel());
+                            startActivity(intent);
+                            trackEvent("click-search-appteka");
+                            break;
+                        }
+                        case 7: {
+                            try {
+                                PackageInfo packageInfo = item.getPackageInfo();
+                                List<String> permissions = Arrays.asList(packageInfo.requestedPermissions);
+                                PermissionsActivity_.intent(getContext())
+                                        .permissions(new PermissionsList(new ArrayList<>(permissions)))
+                                        .start();
+                            } catch (Throwable ex) {
+                                Snackbar.make(recycler, R.string.unable_to_get_permissions, Snackbar.LENGTH_LONG).show();
                             }
-                            case 2: {
-                                TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_EXTRACT));
-                                trackEvent("click-extract-apk");
-                                break;
-                            }
-                            case 3: {
-                                Intent intent = new Intent(getContext(), UploadActivity.class);
-                                intent.putExtra(UploadActivity.UPLOAD_ITEM, item);
-                                startActivity(intent);
-                                trackEvent("click-upload-apk");
-                                break;
-                            }
-                            case 4: {
-                                TaskExecutor.getInstance().execute(new ExportApkTask(getContext(), item, ExportApkTask.ACTION_BLUETOOTH));
-                                trackEvent("click-bluetooth-share");
-                                break;
-                            }
-                            case 5: {
-                                String packageName = item.getPackageName();
-                                openGooglePlay(getContext(), packageName);
-                                trackEvent("click-search-google-play");
-                                break;
-                            }
-                            case 6: {
-                                Intent intent = new Intent(getContext(), DownloadActivity.class);
-                                intent.putExtra(DownloadActivity.STORE_APP_PACKAGE, item.getPackageName());
-                                intent.putExtra(DownloadActivity.STORE_APP_LABEL, item.getLabel());
-                                startActivity(intent);
-                                trackEvent("click-search-appteka");
-                                break;
-                            }
-                            case 7: {
-                                try {
-                                    PackageInfo packageInfo = item.getPackageInfo();
-                                    List<String> permissions = Arrays.asList(packageInfo.requestedPermissions);
-                                    PermissionsActivity_.intent(getContext())
-                                            .permissions(new PermissionsList(new ArrayList<>(permissions)))
-                                            .start();
-                                } catch (Throwable ex) {
-                                    Snackbar.make(recycler, R.string.unable_to_get_permissions, Snackbar.LENGTH_LONG).show();
-                                }
-                                break;
-                            }
-                            case 8: {
-                                setRefreshOnResume();
-                                final Intent intent = new Intent()
-                                        .setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                                        .addCategory(Intent.CATEGORY_DEFAULT)
-                                        .setData(Uri.parse("package:" + item.getPackageName()))
-                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                                trackEvent("click-app-details");
-                                break;
-                            }
-                            case 9: {
-                                setRefreshOnResume();
-                                Uri packageUri = Uri.parse("package:" + item.getPackageName());
-                                Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageUri);
-                                startActivity(uninstallIntent);
-                                trackEvent("click-uninstall-app");
-                                break;
-                            }
+                            break;
+                        }
+                        case 8: {
+                            setRefreshOnResume();
+                            final Intent intent = new Intent()
+                                    .setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                    .addCategory(Intent.CATEGORY_DEFAULT)
+                                    .setData(Uri.parse("package:" + item.getPackageName()))
+                                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            trackEvent("click-app-details");
+                            break;
+                        }
+                        case 9: {
+                            setRefreshOnResume();
+                            Uri packageUri = Uri.parse("package:" + item.getPackageName());
+                            Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageUri);
+                            startActivity(uninstallIntent);
+                            trackEvent("click-uninstall-app");
+                            break;
                         }
                     }
                 }).show();
