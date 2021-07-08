cce20a37f19527327a95eaae16b972cac819b945
Igor Solkin <inbox@tomclaw.com>
2021-07-08 19:11:24 +0300

    Add installed apps update ability

--- app/src/main/java/com/tomclaw/appsend/main/local/AppItemViewHolder.java
+++ app/src/main/java/com/tomclaw/appsend/main/local/AppItemViewHolder.java
@@ -1,6 +1,9 @@
 package com.tomclaw.appsend.main.local;
 
+import static com.tomclaw.appsend.util.TimeHelper.timeHelper;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,12 +12,11 @@ import com.tomclaw.appsend.R;
 import com.tomclaw.appsend.core.GlideApp;
 import com.tomclaw.appsend.main.adapter.files.FileViewHolder;
 import com.tomclaw.appsend.main.adapter.files.FilesListener;
+import com.tomclaw.appsend.main.download.DownloadActivity;
 import com.tomclaw.appsend.main.item.AppItem;
 import com.tomclaw.appsend.util.FileHelper;
 
 import java.util.concurrent.TimeUnit;
-
-import static com.tomclaw.appsend.util.TimeHelper.timeHelper;
 
 public class AppItemViewHolder extends FileViewHolder<AppItem> {
 
@@ -25,6 +27,7 @@ public class AppItemViewHolder extends FileViewHolder<AppItem> {
     private TextView appUpdateTime;
     private TextView appSize;
     private View badgeNew;
+    private View updateButton;
 
     public AppItemViewHolder(View itemView) {
         super(itemView);
@@ -35,18 +38,14 @@ public class AppItemViewHolder extends FileViewHolder<AppItem> {
         appUpdateTime = itemView.findViewById(R.id.app_update_time);
         appSize = itemView.findViewById(R.id.app_size);
         badgeNew = itemView.findViewById(R.id.badge_new);
+        updateButton = itemView.findViewById(R.id.button_update);
     }
 
     @Override
     public void bind(final AppItem item, boolean isLast, final FilesListener<AppItem> listener) {
         Context context = itemView.getContext();
         if (listener != null) {
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    listener.onClick(item);
-                }
-            });
+            itemView.setOnClickListener(v -> listener.onClick(item));
         }
 
         try {
@@ -69,6 +68,15 @@ public class AppItemViewHolder extends FileViewHolder<AppItem> {
         long appInstallDelay = System.currentTimeMillis() - item.getFirstInstallTime();
         boolean isNewApp = appInstallDelay > 0 && appInstallDelay < TimeUnit.DAYS.toMillis(1);
         badgeNew.setVisibility(isNewApp ? View.VISIBLE : View.GONE);
+
+        updateButton.setVisibility(item.getUpdate() != null ? View.VISIBLE : View.GONE);
+        updateButton.setOnClickListener(v -> {
+            Intent intent = new Intent(context, DownloadActivity.class)
+                    .putExtra(DownloadActivity.STORE_APP_ID, item.getUpdate().getAppId())
+                    .putExtra(DownloadActivity.STORE_APP_LABEL, item.getUpdate().getLabel())
+                    .putExtra(DownloadActivity.STORE_FINISH_ONLY, true);
+            context.startActivity(intent);
+        });
     }
 
 }
