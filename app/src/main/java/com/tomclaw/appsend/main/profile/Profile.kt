88a6e49913fa86e9068cfe8d973fdadc4874c1a0
Igor Solkin <inbox@tomclaw.com>
2022-05-06 00:59:20 +0300

    Replace profile manual parceling with parcelize

---
+++ app/src/main/java/com/tomclaw/appsend/main/profile/Profile.kt
@@ -0,0 +1,38 @@
+package com.tomclaw.appsend.main.profile
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import com.tomclaw.appsend.dto.UserIcon
+
+@Parcelize
+data class Profile(
+    @SerializedName("user_id")
+    val userId: Int = 0,
+    @SerializedName("user_icon")
+    val userIcon: UserIcon? = null,
+    @SerializedName("join_time")
+    val joinTime: Long = 0,
+    @SerializedName("last_seen")
+    val lastSeen: Long = 0,
+    @SerializedName("role")
+    val role: Int = 0,
+    @SerializedName("mentor_id")
+    val mentorId: Int = 0,
+    @SerializedName("files_count")
+    val filesCount: Int = 0,
+    @SerializedName("total_downloads")
+    val totalDownloads: Int = 0,
+    @SerializedName("msg_count")
+    val msgCount: Int = 0,
+    @SerializedName("ratings_count")
+    val ratingsCount: Int = 0,
+    @SerializedName("mentor_of_count")
+    val moderatorsCount: Int = 0,
+    @SerializedName("name")
+    val name: String? = null,
+    @SerializedName("is_registered")
+    val isRegistered: Boolean = false,
+    @SerializedName("url")
+    val url: String? = null,
+) : Parcelable
