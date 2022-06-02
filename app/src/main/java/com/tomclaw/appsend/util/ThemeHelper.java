af064a334c71d2c23a9443f20f20fbcfee86e5b5
Igor Solkin <inbox@tomclaw.com>
2022-06-03 00:58:04 +0300

    Fix toolbar colors tint

--- app/src/main/java/com/tomclaw/appsend/util/ThemeHelper.java
+++ app/src/main/java/com/tomclaw/appsend/util/ThemeHelper.java
@@ -1,10 +1,7 @@
 package com.tomclaw.appsend.util;
 
-import static com.tomclaw.appsend.util.ColorsKt.getAttributedColor;
-
 import android.app.Activity;
 
-import com.jaeger.library.StatusBarUtil;
 import com.tomclaw.appsend.R;
 
 /**
@@ -16,15 +13,6 @@ public class ThemeHelper {
         boolean isDarkTheme = PreferenceHelper.isDarkTheme(activity);
         activity.setTheme(isDarkTheme ? R.style.AppThemeBlack : R.style.AppTheme);
         return isDarkTheme;
-    }
-
-    public static void updateStatusBar(Activity activity) {
-        int toolbarColor = getAttributedColor(activity, R.attr.toolbar_background);
-        updateStatusBar(activity, toolbarColor);
-    }
-
-    public static void updateStatusBar(Activity activity, int color) {
-        StatusBarUtil.setColor(activity, color);
     }
 
 }
