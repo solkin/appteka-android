6ea96f909a019fad2dbba31e6afe1cb00bf4f910
Igor Solkin <inbox@tomclaw.com>
2023-07-03 22:45:50 +0300

    Fix details screen duplication after description editing

--- app/src/main/java/com/tomclaw/appsend/screen/details/DetailsActivity.kt
+++ app/src/main/java/com/tomclaw/appsend/screen/details/DetailsActivity.kt
@@ -118,6 +118,15 @@ class DetailsActivity : AppCompatActivity(), DetailsPresenter.DetailsRouter {
         presenter.attachView(view)
     }
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        overridePendingTransition(0, 0)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        finish()
+        overridePendingTransition(0, 0)
+        startActivity(intent)
+    }
+
     override fun onBackPressed() {
         presenter.onBackPressed()
     }
