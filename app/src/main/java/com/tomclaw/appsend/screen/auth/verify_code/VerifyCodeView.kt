2a2b0248816f836e0d9b13cc4a0f3bfe305bb503
Igor Solkin <inbox@tomclaw.com>
2023-09-19 23:13:55 +0300

    Add verification code view implementation

--- app/src/main/java/com/tomclaw/appsend/screen/auth/verify_code/VerifyCodeView.kt
+++ app/src/main/java/com/tomclaw/appsend/screen/auth/verify_code/VerifyCodeView.kt
@@ -1,9 +1,18 @@
 package com.tomclaw.appsend.screen.auth.verify_code
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
 import com.jakewharton.rxrelay3.PublishRelay
 import com.tomclaw.appsend.R
+import com.tomclaw.appsend.util.hide
+import com.tomclaw.appsend.util.hideWithAlphaAnimation
+import com.tomclaw.appsend.util.show
+import com.tomclaw.appsend.util.showWithAlphaAnimation
 import io.reactivex.rxjava3.core.Observable
 
 interface VerifyCodeView {
@@ -12,35 +21,70 @@ interface VerifyCodeView {
 
     fun showContent()
 
-    fun showError()
+    fun showNameInput()
 
-    fun hideError()
+    fun hideNameInput()
+
+    fun showError()
 
     fun navigationClicks(): Observable<Unit>
 
 }
 
-class VerifyCodeViewImpl(view: View) : VerifyCodeView {
+class VerifyCodeViewImpl(private val view: View) : VerifyCodeView {
 
     private val context = view.context
     private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+    private val codeInput: EditText = view.findViewById(R.id.name_block)
+    private val nameBlock: View = view.findViewById(R.id.code_input)
+    private val nameInput: EditText = view.findViewById(R.id.name_input)
+    private val submitButton: Button = view.findViewById(R.id.submit_button)
+    private val overlayProgress: View = view.findViewById(R.id.overlay_progress)
 
     private val navigationRelay = PublishRelay.create<Unit>()
+    private val codeChangedRelay = PublishRelay.create<String>()
+    private val nameChangedRelay = PublishRelay.create<String>()
+    private val submitRelay = PublishRelay.create<Unit>()
 
     init {
+        toolbar.setTitle(R.string.verify_email)
         toolbar.setNavigationOnClickListener { navigationRelay.accept(Unit) }
+
+        codeInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                codeChangedRelay.accept(s.toString())
+            }
+        })
+        nameInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                nameChangedRelay.accept(s.toString())
+            }
+        })
+        submitButton.setOnClickListener { submitRelay.accept(Unit) }
     }
 
     override fun showProgress() {
+        overlayProgress.showWithAlphaAnimation(animateFully = true)
     }
 
     override fun showContent() {
+        overlayProgress.hideWithAlphaAnimation(animateFully = false)
+    }
+
+    override fun showNameInput() {
+        nameBlock.show()
+    }
+
+    override fun hideNameInput() {
+        nameBlock.hide()
     }
 
     override fun showError() {
-    }
-
-    override fun hideError() {
+        Snackbar.make(view, R.string.error_verifying_code, Snackbar.LENGTH_LONG).show()
     }
 
     override fun navigationClicks(): Observable<Unit> = navigationRelay
