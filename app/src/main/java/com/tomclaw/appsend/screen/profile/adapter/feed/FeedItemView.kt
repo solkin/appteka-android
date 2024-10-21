2c15852c5b9ae7b47e7812b3cd69f4790ecee3c8
Igor Solkin <inbox@tomclaw.com>
2024-10-21 22:24:48 +0300

    Colorize feed, subs and pubs count based on count

--- app/src/main/java/com/tomclaw/appsend/screen/profile/adapter/feed/FeedItemView.kt
+++ app/src/main/java/com/tomclaw/appsend/screen/profile/adapter/feed/FeedItemView.kt
@@ -6,6 +6,8 @@ import com.avito.konveyor.adapter.BaseViewHolder
 import com.avito.konveyor.blueprint.ItemView
 import com.tomclaw.appsend.R
 import com.tomclaw.appsend.util.bind
+import com.tomclaw.appsend.util.getAttributedColor
+import com.tomclaw.appsend.util.getColor
 
 interface FeedItemView : ItemView {
 
@@ -25,6 +27,7 @@ interface FeedItemView : ItemView {
 
 class FeedItemViewHolder(view: View) : BaseViewHolder(view), FeedItemView {
 
+    private val context = view.context
     private val feedBlockView: View = view.findViewById(R.id.feed_block)
     private val subsBlockView: View = view.findViewById(R.id.subs_block)
     private val pubsBlockView: View = view.findViewById(R.id.pubs_block)
@@ -44,14 +47,17 @@ class FeedItemViewHolder(view: View) : BaseViewHolder(view), FeedItemView {
 
     override fun setFeedCount(count: Int) {
         feedCountView.bind(count.toString())
+        feedCountView.setTextColor(getAttributedColor(context, getTextColor(count)))
     }
 
     override fun setSubsCount(count: Int) {
         subsCountView.bind(count.toString())
+        subsCountView.setTextColor(getAttributedColor(context, getTextColor(count)))
     }
 
     override fun setPubsCount(count: Int) {
         pubsCountView.bind(count.toString())
+        pubsCountView.setTextColor(getAttributedColor(context, getTextColor(count)))
     }
 
     override fun setOnFeedClickListener(listener: (() -> Unit)?) {
@@ -70,6 +76,12 @@ class FeedItemViewHolder(view: View) : BaseViewHolder(view), FeedItemView {
         this.feedClickListener = null
         this.subsClickListener = null
         this.pubsClickListener = null
+    }
+
+    private fun getTextColor(count: Int) = if (count > 0) {
+        getAttributedColor(context, R.attr.text_primary_color)
+    } else {
+        getColor(R.color.primary_dark_color, context)
     }
 
 }
