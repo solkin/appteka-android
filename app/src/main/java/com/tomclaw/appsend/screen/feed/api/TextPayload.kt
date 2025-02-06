1e332eaaf079d9651e6b549373dabd23f181b5aa
Igor Solkin <inbox@tomclaw.com>
2025-02-06 22:43:46 +0300

    Fix text payload field names

--- app/src/main/java/com/tomclaw/appsend/screen/feed/api/TextPayload.kt
+++ app/src/main/java/com/tomclaw/appsend/screen/feed/api/TextPayload.kt
@@ -1,8 +1,11 @@
 package com.tomclaw.appsend.screen.feed.api
 
+import com.google.gson.annotations.SerializedName
 import com.tomclaw.appsend.dto.Screenshot
 
 data class TextPayload(
+    @SerializedName("screenshots")
     val screenshots: List<Screenshot>,
+    @SerializedName("text")
     val text: String,
 ): PostPayload
