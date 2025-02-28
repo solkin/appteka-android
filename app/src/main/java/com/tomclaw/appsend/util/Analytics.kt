31177cbcd6fd2cc4ba35af37ecb2ac328dfc324b
Igor Solkin <inbox@tomclaw.com>
2025-03-01 02:05:07 +0300

    Remove retired Microsoft AppCenter

--- app/src/main/java/com/tomclaw/appsend/util/Analytics.kt
+++ app/src/main/java/com/tomclaw/appsend/util/Analytics.kt
@@ -1,11 +1,6 @@
 package com.tomclaw.appsend.util
 
 import android.app.Application
-import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Bundle
-import android.text.TextUtils
-import com.microsoft.appcenter.AppCenter
 import com.tomclaw.appsend.analytics.Bananalytics
 
 interface Analytics {
@@ -22,43 +17,11 @@ class AnalyticsImpl(
 ) : Analytics {
 
     override fun register() {
-        val appIdentifier = getAppIdentifier(app.applicationContext)
-        AppCenter.start(
-            app, appIdentifier,
-            com.microsoft.appcenter.analytics.Analytics::class.java,
-            com.microsoft.appcenter.crashes.Crashes::class.java
-        )
         bananalytics.flushEvents()
     }
 
-    override fun trackEvent(name: String) {
-        com.microsoft.appcenter.analytics.Analytics.trackEvent(name)
+    override fun trackEvent(name: String ) {
         bananalytics.trackEvent(name)
     }
 
-    private fun getAppIdentifier(context: Context): String? {
-        val appID = getManifestString(context, APP_IDENTIFIER_KEY)
-        if (TextUtils.isEmpty(appID)) {
-            throw RuntimeException("AppCenter app identifier was not configured correctly in manifest or build configuration.")
-        }
-        return appID
-    }
-
-    private fun getManifestString(context: Context, key: String): String? {
-        return getManifestBundle(context).getString(key)
-    }
-
-    private fun getManifestBundle(context: Context): Bundle {
-        return try {
-            context.packageManager.getApplicationInfo(
-                context.packageName,
-                PackageManager.GET_META_DATA
-            ).metaData
-        } catch (e: PackageManager.NameNotFoundException) {
-            throw RuntimeException(e)
-        }
-    }
-
 }
-
-const val APP_IDENTIFIER_KEY = "appcenter.app_identifier"
