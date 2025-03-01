44bc7ce5da79189963a0f7ba262050a3cbd38512
Igor Solkin <inbox@tomclaw.com>
2025-03-01 13:52:31 +0300

    Replace Sentry with AppMetrica

--- app/src/main/java/com/tomclaw/appsend/util/Analytics.kt
+++ app/src/main/java/com/tomclaw/appsend/util/Analytics.kt
@@ -2,6 +2,8 @@ package com.tomclaw.appsend.util
 
 import android.app.Application
 import com.tomclaw.appsend.analytics.Bananalytics
+import io.appmetrica.analytics.AppMetrica
+import io.appmetrica.analytics.AppMetricaConfig
 
 interface Analytics {
 
@@ -16,11 +18,17 @@ class AnalyticsImpl(
     private val bananalytics: Bananalytics,
 ) : Analytics {
 
+    private val Y_API_KEY = "30ce63a5-985e-4274-9c1f-69e4834c0a32"
+
     override fun register() {
+        val config = AppMetricaConfig.newConfigBuilder(Y_API_KEY).build()
+        AppMetrica.activate(app, config)
+        AppMetrica.sendEventsBuffer()
         bananalytics.flushEvents()
     }
 
-    override fun trackEvent(name: String ) {
+    override fun trackEvent(name: String) {
+        AppMetrica.reportEvent(name)
         bananalytics.trackEvent(name)
     }
 
