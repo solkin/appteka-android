4ed5bb7f45178cc4eb34c29f019749fd009f7245
Igor Solkin <inbox@tomclaw.com>
2025-03-17 20:28:04 +0300

    Add screenshots to post converter

--- app/src/main/java/com/tomclaw/appsend/screen/post/PostConverter.kt
+++ app/src/main/java/com/tomclaw/appsend/screen/post/PostConverter.kt
@@ -2,6 +2,9 @@ package com.tomclaw.appsend.screen.post
 
 import com.avito.konveyor.blueprint.Item
 import com.tomclaw.appsend.screen.post.dto.PostScreenshot
+import com.tomclaw.appsend.screen.upload.adapter.screen_append.ScreenAppendItem
+import com.tomclaw.appsend.screen.upload.adapter.screen_image.ScreenImageItem
+import com.tomclaw.appsend.screen.upload.adapter.screenshots.ScreenshotsItem
 
 interface PostConverter {
 
@@ -18,7 +21,29 @@ class PostConverterImpl() : PostConverter {
         screenshots: List<PostScreenshot>,
         test: String
     ): List<Item> {
-        TODO("Not yet implemented")
+        var id: Long = 1
+        val items = ArrayList<Item>()
+
+        items += ScreenshotsItem(
+            id = id++,
+            items = screenshots.map {
+                ScreenImageItem(
+                    id = it.longId(),
+                    original = it.original,
+                    preview = it.preview,
+                    width = it.width,
+                    height = it.height,
+                    remote = it.remote()
+                )
+            } + ScreenAppendItem(id++)
+        )
+
+        return items
     }
+
+    private fun PostScreenshot.longId(): Long =
+        (scrId?.hashCode() ?: original.toString().hashCode()).toLong()
+
+    private fun PostScreenshot.remote(): Boolean = scrId != null
 
 }
