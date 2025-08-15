3ed486d5cff3a201423d36529f483847b0643870
Igor Solkin <inbox@tomclaw.com>
2025-08-15 23:12:53 +0300

    Remove legacy unpublish screen

--- app/src/main/java/com/tomclaw/appsend/core/StoreService.java
+++ app/src/main/java/com/tomclaw/appsend/core/StoreService.java
@@ -2,26 +2,15 @@ package com.tomclaw.appsend.core;
 
 import com.tomclaw.appsend.main.dto.ApiResponse;
 import com.tomclaw.appsend.main.store.ListResponse;
-import com.tomclaw.appsend.main.unpublish.UnpublishResponse;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
  * Created by solkin on 23.09.17.
  */
 public interface StoreService {
-
-    @FormUrlEncoded
-    @POST("api/1/app/unpublish")
-    Call<ApiResponse<UnpublishResponse>> unpublish(
-            @Field("app_id") String fileId,
-            @Field("reason") String reason
-    );
 
     @GET("api/1/user/app/list")
     Call<ApiResponse<ListResponse>> listUserFiles(
