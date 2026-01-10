cb43e50de122170d239f9c4541b8dbda7e1e0474
Igor Solkin <igor@citron.dev>
2026-01-11 00:29:25 +0300

    Up build number

--- app/src/main/java/com/tomclaw/appsend/core/Config.kt
+++ app/src/main/java/com/tomclaw/appsend/core/Config.kt
@@ -3,4 +3,4 @@ package com.tomclaw.appsend.core
 const val HOST_URL: String = "https://appteka.store"
 const val STAND_BY_HOST_URL: String = "https://tomclaw.com"
 const val BANANALYTICS_URL: String = "https://bnn.appteka.store"
-const val BANANALYTICS_API_KEY: String = "bnn_tojZqetHHkRfdCbasqiIMcunqhPACSg1"
+const val BANANALYTICS_API_KEY: String = "bnn_y2zEuodSTELhSoKeh_4ldiLFuJilThQ9"
